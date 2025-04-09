f4c868fe7c8f0031feea192550bf0ec0ebed6961
Ilyas <79158764+mr-ceri-mrum@users.noreply.github.com>
2025-04-09 13:46:07 +0500

    Add index file for all UI components

---
+++ src/components/UI/index.js
@@ -0,0 +1,10 @@
+/**
+ * Экспорт всех UI компонентов из одного файла
+ * Позволяет импортировать все компоненты через один импорт:
+ * import { Button, Container, Section, SectionTitle, Card } from 'components/UI';
+ */
+export { default as Button } from './Button';
+export { default as Container } from './Container';
+export { default as Section } from './Section';
+export { default as SectionTitle } from './SectionTitle';
+export { default as Card } from './Card';
