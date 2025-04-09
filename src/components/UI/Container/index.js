14c7e9a0fa30623a071374e206db37bbd5615224
Ilyas <79158764+mr-ceri-mrum@users.noreply.github.com>
2025-04-09 13:45:01 +0500

    Add index file for Container component

---
+++ src/components/UI/Container/index.js
@@ -0,0 +1,5 @@
+/**
+ * Экспорт компонента Container для более удобного импорта
+ * Позволяет использовать import { Container } from 'components/UI/Container'
+ */
+export { default } from './Container';
