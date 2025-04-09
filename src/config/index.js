4b6ffe5f7a4cbbe9114d82cd70c14096bd4b1f1f
Ilyas <79158764+mr-ceri-mrum@users.noreply.github.com>
2025-04-09 13:48:14 +0500

    Add index file for config directory

---
+++ src/config/index.js
@@ -0,0 +1,11 @@
+/**
+ * Конфигурационные файлы приложения
+ * Единая точка входа для всех конфигураций
+ */
+import theme from './theme';
+import navigation from './navigation';
+
+export {
+  theme,
+  navigation
+};
